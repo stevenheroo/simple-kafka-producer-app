e4b7877f30edbf6f7213680927a1e1fd0af86252
stevenheroo <stevenfianu99@gmail.com>
2022-09-14 15:44:12 +0000

    call back when send func is called

--- src/main/java/com/github/skep/kafkaproducer/tutorial1/ProducerDemo.java
+++ src/main/java/com/github/skep/kafkaproducer/tutorial1/ProducerDemo.java
@@ -1,10 +1,9 @@
 package com.github.skep.kafkaproducer.tutorial1;
 
-import com.sun.org.slf4j.internal.LoggerFactory;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
@@ -12,6 +11,8 @@ public class ProducerDemo {
     private static final String BOOTSTRAP_SERVER_PORT = "127.0.0.1:9092";
 
     public static void main(String[] args) {
+        Logger LOG = LoggerFactory.getLogger(ProducerDemo.class);
+
         //create properties
         Properties props = new Properties();
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER_PORT);
@@ -23,10 +24,22 @@ public class ProducerDemo {
 
         //create a record
         ProducerRecord<String, String> record =
-                new ProducerRecord<>("first-topic", "hello world2");
+                new ProducerRecord<>("first-topic", "hello world3");
 
         //send data
-        producer.send(record);
-        producer.close();
+        producer.send(record, (recordMetadata, e) -> {
+            if (e != null) {
+                LOG.info("Successful");
+            }
+            else {
+                LOG.info("\nMetadata Response\n"+
+                        "TOPIC: " + recordMetadata.topic() + "\n"+
+                        "PARTITION: " + recordMetadata.partition()+ "\n"+
+                        "OFFSET: " + recordMetadata.offset() + "\n" +
+                        "TIMESTAMP: " + recordMetadata.timestamp()
+                );
+            }
+        });
+        producer.flush();
     }
 }
